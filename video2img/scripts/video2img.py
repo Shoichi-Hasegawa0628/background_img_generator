06f4e324c19f5b84b9e65bbbbf0c543815677ed6
Shoichi-Hasegawa0628 <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2021-12-11 17:24:54 +0900

    WIP: rosbag2img

--- video2img/scripts/video2img.py
+++ video2img/scripts/video2img.py
@@ -1,25 +1,40 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Standard Library
 import cv2
 import os
 
-def save_4times_frames(video_path, dir_path):
-    cap = cv2.VideoCapture(video_path)
+# Third Party
+import rospy
 
-    if not cap.isOpened():
-        return
 
-    os.makedirs(dir_path, exist_ok=True)
-    #base_path = os.path.join(dir_path, basename)
-    #digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
-    n = 0
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            if n%4 == 0:
-                cv2.imwrite('./data/img/{}.png'.format(int(n/4)), frame)
-            n += 1
-        else:
+class Video2Img():
+
+    def __init__(self):
+        pass
+
+    def save_4times_frames(self, video_path, dir_path):
+        cap = cv2.VideoCapture(video_path)
+
+        if not cap.isOpened():
             return
 
-save_4times_frames('./data/video/background.mp4', './data/img')
+        os.makedirs(dir_path, exist_ok=True)
+        # base_path = os.path.join(dir_path, basename)
+        # digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+        n = 0
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                if n % 4 == 0:
+                    cv2.imwrite(dir_path + '/{}.png'.format(int(n / 4)), frame)
+                n += 1
+            else:
+                return
+
+
+if __name__ == "__main__":
+    rospy.init_node('video2img')
+    video2img = Video2Img()
+    video2img.save_4times_frames('../../data/video/background.mp4', '../../data/img')
