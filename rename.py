914b16a88225a345a576c9fca79439777530505e
Shoichi-Hasegawa0628 <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2022-01-23 16:30:00 +0900

    Fixed the number of image

--- rename.py
+++ rename.py
@@ -3,13 +3,14 @@
 import cv2
 import os
 
-img_path = "/root/HSR/background_img/living/img"
+img_path = "/root/HSR/catkin_ws/src/spco2_boo/rgiro_spco2_slam/data/raw"
 # print(sum(os.path.isfile(os.path.join(img_path, name)) for name in os.listdir(img_path)))
 img_num = sum(os.path.isfile(os.path.join(img_path, name)) for name in os.listdir(img_path))
 
 for i in range(img_num):
     img = cv2.imread(img_path + "/{}.png".format(i))
-    cv2.imwrite(img_path + "/{}.png".format(604 + (i + 1)), img)
+    cv2.imwrite(img_path + "/rename" + "/{}.jpg".format(70 + (i + 1)), img)
+    # cv2.imwrite(img_path + "/rename" + "/{}.jpg".format(i + 1), img)
 
 
 
